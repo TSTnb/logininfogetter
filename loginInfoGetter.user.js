bb22149d05b7c98b05f3d0dad5220a4910768da8
morningpee <morningpee2@gmail.com>
2018-01-13 12:32:10 -0600

    initial script

---
+++ loginInfoGetter.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name Login Info Getter
+// @namespace logininfogetter
+// @description gets sessionId, sessionToken, and timestamp
+// @include http://*.tetrisfriends.com/games/Live/game.php
+// @grant none
+// @run-at document-end
+// @version 0.0.1
+// @author morningpee
+// ==/UserScript==
+
+function buildFlashVarsParamString()
+{
+    var flashVars = new Object();
+
+    var flashVarsRequest = new XMLHttpRequest();
+    flashVarsRequest.addEventListener('load', function(){ try{ haveFlashVars(this.responseText, flashVars); } catch(err){alert(err + "\n" + err.stack);} } );
+
+    var ASYNCHRONOUS_REQUEST = true;
+    flashVarsRequest.open('GET', location.href, ASYNCHRONOUS_REQUEST);
+    flashVarsRequest.send();
+}
+
+function haveFlashVars(responseText, flashVars)
+{
+    var rawFlashVars = responseText.match(/flashVars.*?=.*?({[\s\S]*?})/)[1];
+
+    flashVars.sessionId = rawFlashVars.match(/sessionId.*?:.*?encodeURIComponent\('(.*?)'\)/)[1];
+    flashVars.sessionToken = rawFlashVars.match(/sessionToken.*?:.*?encodeURIComponent\('(.*?)'\)/)[1];
+    flashVars.timestamp = rawFlashVars.match(/timestamp.*?:.*?(\d+)/)[1];
+    document.body.appendChild(document.createElement('textArea')).textContent = 'sessionId: ' + flashVars.sessionId + "\nsessionToken: " + flashVars.sessionToken + "\ntimestamp: " + flashVars.timestamp;
+}
+
+buildFlashVarsParamString();
